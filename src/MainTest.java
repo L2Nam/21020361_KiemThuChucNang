3e47b65737672703e563f3c1224ea27ddb0e158c
L2Nam <lenamlhn234@gmail.com>
2024-03-29 10:57:51 +0700

    add_kiemthubang

--- src/MainTest.java
+++ src/MainTest.java
@@ -4,63 +4,73 @@ import static org.junit.Assert.assertEquals;
 public class MainTest {
 
     // PHUONG PHAP BANG QUYET DINH
-     @Test
-     public void testBangQuyetDinh1() {
-         assertEquals("Invalid error", Main.tinhHocPhi(1, -2));
-     }
+    @Test
+    public void testBangQuyetDinh1() {
+        assertEquals("Invalid error", Main.tinhHocPhi(1, -2));
+    }
 
-     @Test
-     public void testBangQuyetDinh2() {
-         assertEquals("750k", Main.tinhHocPhi(1, 3));
+    @Test
+    public void testBangQuyetDinh2() {
+        assertEquals("750k", Main.tinhHocPhi(1, 3));
 
-     }
+    }
 
-     @Test
-     public void testBangQuyetDinh3() {
-         assertEquals("1940k", Main.tinhHocPhi(1, 8));
-     }
+    @Test
+    public void testBangQuyetDinh3() {
+        assertEquals("1940k", Main.tinhHocPhi(1, 8));
+    }
 
-     @Test
-     public void testBangQuyetDinh4() {
-         assertEquals("3280k", Main.tinhHocPhi(1, 14));
-     }
+    @Test
+    public void testBangQuyetDinh4() {
+        assertEquals("3280k", Main.tinhHocPhi(1, 14));
+    }
 
-     @Test
-     public void testBangQuyetDinh5() {
-         assertEquals("4100k", Main.tinhHocPhi(1, 18));
-     }
+    @Test
+    public void testBangQuyetDinh5() {
+        assertEquals("4100k", Main.tinhHocPhi(1, 18));
+    }
 
-     @Test
-     public void testBangQuyetDinh6() {
-         assertEquals("Invalid error", Main.tinhHocPhi(2, -3));
-     }
+    @Test
+    public void testBangQuyetDinh6() {
+        assertEquals("Invalid error", Main.tinhHocPhi(2, -3));
+    }
 
-     @Test
-     public void testBangQuyetDinh7() {
-         assertEquals("2400k", Main.tinhHocPhi(2, 12));
-     }
+    @Test
+    public void testBangQuyetDinh7() {
+        assertEquals("800k", Main.tinhHocPhi(2, 4));
+    }
 
-     @Test
-     public void testBangQuyetDinh8() {
-         assertEquals("3300k", Main.tinhHocPhi(2, 17));
-     }
+    @Test
+    public void testBangQuyetDinh8() {
+        assertEquals("1600k", Main.tinhHocPhi(2, 8));
+    }
 
-     @Test
-     public void testBangQuyetDinh9() {
-         assertEquals("Invalid error", Main.tinhHocPhi(3, -6));
-     }
+    @Test
+    public void testBangQuyetDinh9() {
+        assertEquals("2400k", Main.tinhHocPhi(2, 12));
+    }
 
-     @Test
-     public void testBangQuyetDinh10() {
-         assertEquals("1050k", Main.tinhHocPhi(3, 7));
-     }
+    @Test
+    public void testBangQuyetDinh10() {
+        assertEquals("3300k", Main.tinhHocPhi(2, 17));
+    }
 
-     @Test
-     public void testBangQuyetDinh11() {
-         assertEquals("Invalid error", Main.tinhHocPhi(4, 9));
-     }
+    @Test
+    public void testBangQuyetDinh11() {
+        assertEquals("Invalid error", Main.tinhHocPhi(3, -6));
+    }
 
-     // PHUONG PHAP PHAN HOACH TUONG DUONG
+    @Test
+    public void testBangQuyetDinh12() {
+        assertEquals("1050k", Main.tinhHocPhi(3, 7));
+    }
+
+    @Test
+    public void testBangQuyetDinh13() {
+        assertEquals("Invalid error", Main.tinhHocPhi(4, 9));
+    }
+
+    // PHUONG PHAP PHAN HOACH TUONG DUONG
 
     @Test
     public void testPhanHoachTuongDuong1() {
